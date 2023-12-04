02cc68186289b96fd988a2e7d950a7d3d0d32137
nittinkumarhr <127714499+nittinkumarhr@users.noreply.github.com>
2023-12-04 10:44:56 +0530

    data types

---
+++ 02datatypes.js
@@ -0,0 +1,45 @@
+'use strict'//# :=> treat all js code  as newer version
+
+
+// alert(3+8) :=> we are useing node js not Browser 
+
+ console.log (3+ 
+    5
+ )//#endregion code readabilty should be high 
+
+
+ let name ='nitin'
+ let age =19
+ let islogin = true
+
+//  number => 2 to power 53
+// bigint
+// string =>"nitn"
+
+// boolean => true/flase
+// null =>  standlone value /emty varible 
+//  undefind =>  no vaule asgin
+// ! symbol 
+
+
+
+
+//  Object 
+
+
+// console.log(typeof "nitn")
+console.table(['String :=>'+typeof'nitin','number :=>'+ typeof 123,'Undefinded varibles :=>'+ typeof undefined ,'null value :=>'+typeof null])
+
+
+
+/*NOTE - note : output of the program 
+
+─────────┬────────────────────────────────────┐
+│ (index) │               Values               │
+├─────────┼────────────────────────────────────┤
+│    0    │         'String :=>string'         │
+│    1    │         'number :=>number'         │
+│    2    │ 'Undefinded varibles :=>undefined' │
+│    3    │       'null value :=>object'       │
+└─────────┴────────────────────────────────────┘
+*/
