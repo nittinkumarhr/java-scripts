b616ea0d53dcc950e0fa77df08f32a5a9ef4f49f
Nitin kumar <127714499+nittinkumarhr@users.noreply.github.com>
2024-06-24 11:04:10 +0000

    basic js

--- week1/basic.js
+++ week1/basic.js
@@ -1,1 +1,35 @@
-console.log("hello world")
+const accountId =1234433;//#endregion:=>   NO Change the value of varible 
+let accountPassword ='12344';//#endregion:=> NO Change the value of varible 
+var accountEmail ='nitin@gmail.com';//#endregion:=> Change the value of varible 
+accountCity ='Bijnor';//#endregion:=> Change the value of varible 
+let accountState;//#region  undefined 
+
+// accountId =2; no change the value of varible 
+accountEmail='nk@gmail.com';
+accountPassword='778889';
+accountCity='noida';
+console.log('nitin kumar')
+// xsxsxs
+console.log(accountId)//#endregion print the only one varible 
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+/*NOTE - 
+NO us the  var  because  of issue in the blouck scope and  functional scope 
+// *
+/*!SECTION
+out put of the code 
+┌─────────┬────────────────┐
+│ (index) │     Values     │
+├─────────┼────────────────┤
+│    0    │    1234433     │
+│    1    │ 'nk@gmail.com' │
+│    2    │    '778889'    │
+│    3    │    'noida'     │
+│    4    │   undefined    │
+└─────────┴────────────────┘
+*/
+console.log('nk@gmail');
+
+
+
+
+
