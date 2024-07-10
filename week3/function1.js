dd4c82f859bc887945513f3746e8e7656f16e68d
Nitin kumar <127714499+nittinkumarhr@users.noreply.github.com>
2024-07-10 09:40:23 +0000

    Arrow functions

--- week3/function1.js
+++ week3/function1.js
@@ -30,6 +30,79 @@ function addsum(num1, num2){
 
 function multiplefun(...num){
     return num
+}   
+// console.log(multiplefun(12,23,45,43,21))// return the first ietms
+
+
+//-------- -----Arrow function ------------------------------------------------------------------------------
+// this keybord about the cuernt context 
+
+const user={
+    username:"nitin",
+    price:999,
+    welcome: function(){
+        console.log(`${this.username} , welcome to the chat AI`)
+        console.log(this)// return the object of context of user -> { username: 'nitin', price: 999, welcome: [Function: welcome] }
+
+    }
 }
-console.log(multiplefun(12,23,45,43,21))// return the first ietms
+
+// user.welcome()// return the : nitin , welcome to the chat AI
+// user.username="Ram ji"
+// user.welcome()/// return the : Ram ji , welcome to the chat AI
+
+// user.welcome()
+
+// console.log(this)// node envroiments  return emtpy object like this->{}
+
+// function nk(){
+//     console.log(this)
+// }
+// nk()
+
+// function nk(){
+//     let username='nitin  kmar'
+//     console.log(this.username)// return undefined it is working only objects 
+// }
+// nk()
+
+
+// const code=function(){
+//     console.log("first type of declartions ")
+//     let username='nitin  kmar'
+//     console.log(this.username)// return undefined it is working only objects 
+// }
+// code()
+
+
+
+// const code=()=>{
+//     console.log("Arrow function  declartions ")
+//     let username='nitin  kmar'
+//     console.log(this.username)// return undefined it is working only objects 
+// }
+// code()
+
+// const addTwo=(num1,num2)=>{
+//     return num1+num2
+// }
+// console.log(addTwo(42,22))
+
+
+// const addTwo=(num1,num2)=> (num1+num2)
+// console.log(addTwo(42,22))
+
+
+// ------------------Immediately invoked  function---------------------------------------------
+ // ( ) ( )
+
+//  (function(a, b) {
+//     console.log(a + b);
+// })(5, 10); // Outputs: 15
+
+
+
+
+
+
 
